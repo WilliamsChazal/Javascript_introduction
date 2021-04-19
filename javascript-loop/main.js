dfc1627190555c2037a63c791b910dfd23d99cc0
williams <w.chazal@codeur.online>
2021-04-19 10:14:32 +0200

    loop-JS

---
+++ javascript-loop/main.js
@@ -0,0 +1,43 @@
+/* for (let counter = 0; counter<2000; counter++){
+    console.log(counter);
+} */
+
+/* let counter=0;
+do{
+    counter++;
+    console.log(counter);
+} while(counter<2000); */
+
+/* let str = '';
+
+loop1:
+for (let counter = 0; counter < 2000; counter++) {
+  if (counter === 52||counter === 1998) {
+    continue loop1;
+  } else if (counter === 50){
+break;
+  }
+  str = str + counter;
+}
+
+console.log(str); */
+
+/* let arr = [3, 5, 7,9,15,420];
+
+arr.adrien = 832;
+loop2:
+for (let i in arr) {
+    if(i==2){
+        continue loop2;
+    }
+    
+  console.log(i); 
+}
+loop2:
+for (let i of arr) {
+    if(i==7){
+        continue loop2;
+    }
+  console.log(i); 
+} */
+  
