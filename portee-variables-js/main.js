b927dc8971b86826388038e01f253a7a0a013df4
williams <w.chazal@codeur.online>
2021-04-21 09:37:57 +0200

    Portée des variables

---
+++ portee-variables-js/main.js
@@ -0,0 +1,18 @@
+const coucou = 55;
+console.log(coucou);
+
+function test(){
+    let hello = 24;
+    console.log(hello);
+}
+
+document.getElementById('button').addEventListener('click',test);
+let hello = 'Salut Nicolas';
+console.log(hello);
+
+function truc(){
+    let hello = 32;
+    console.log(hello);
+}
+
+document.getElementById('click').addEventListener('click',truc);
