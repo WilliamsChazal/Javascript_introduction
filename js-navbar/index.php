8b0b012c6829767f7f97e5501b0f10d3ed2a1c71
williams <w.chazal@codeur.online>
2021-05-17 12:31:29 +0200

    js-navbar

---
+++ js-navbar/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Slide Down</title>
+</head>
+<body>
+<div id="navbar">
+ <a href="#home">Home</a>
+ <a href="#news">News</a>
+ <a href="#contact">Contact</a>
+ </div>
+ <div class="lorem">
+ Lorem Ipsum
+"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+
+<br>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget magna varius tellus malesuada luctus et in tellus. Cras iaculis nisi mauris, vitae ullamcorper mauris maximus sit amet. In pellentesque elit vel metus faucibus, tincidunt luctus odio efficitur. Fusce nisi leo, dignissim id mi non, semper hendrerit felis. Phasellus ut urna et dui ultrices convallis. Nulla feugiat dictum aliquam. Nunc sed sollicitudin est. In fringilla nisi quam, at feugiat lorem sollicitudin eu.
+<br>
+Pellentesque at pretium libero, porta commodo nunc. Curabitur quis elit eget lacus maximus suscipit vel in augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed risus libero, luctus id quam sit amet, aliquam vehicula lectus. Cras auctor odio ligula, a scelerisque tellus porta sed. Maecenas eu felis mauris. Praesent dignissim turpis enim, non condimentum turpis consectetur eu. Donec ut justo sed nunc consectetur dapibus. Quisque vel sagittis nisl. Cras luctus interdum fermentum. Sed pretium volutpat leo, nec pellentesque ex vestibulum ac. Nulla a ex tempus, volutpat nibh in, consectetur metus. Praesent velit eros, condimentum in tristique in, faucibus quis ante. Aliquam porta velit lectus, in ullamcorper libero aliquam varius.
+<br>
+Donec in dictum orci. Curabitur lacus urna, imperdiet nec blandit varius, mollis ut nibh. Vivamus non tincidunt arcu. Praesent id consectetur libero. Sed sit amet neque sagittis, tincidunt risus nec, pellentesque nunc. Mauris vitae massa sed ligula sagittis semper ac a magna. Donec scelerisque pulvinar odio et porttitor. Aenean non mauris et odio aliquet sodales at quis libero. Quisque sit amet sapien mi. Cras vestibulum, arcu ut hendrerit aliquet, magna est pellentesque lorem, sed placerat turpis sem vulputate nisi. Suspendisse mollis a massa at aliquam. Quisque bibendum sem enim, vel egestas nulla volutpat et. In eleifend, tellus ac luctus ullamcorper, sem tortor aliquam magna, et congue purus odio quis diam. Nunc id dictum enim, id dictum odio.
+<br>
+Nulla facilisi. Aenean sagittis faucibus tortor, at suscipit dui viverra vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer tempus est maximus nulla suscipit condimentum. Sed ornare quam ipsum, eget rhoncus massa egestas eu. Fusce at sollicitudin purus. Mauris in porttitor sem. Maecenas tristique ut magna id condimentum. Nunc molestie tellus nec velit euismod fermentum.
+<br>
+Fusce consectetur justo quis ullamcorper egestas. Donec mattis ligula ut massa rutrum, vel fringilla ante vestibulum. Maecenas interdum tellus at augue feugiat, eu varius nulla finibus. Aliquam tincidunt sodales lectus nec efficitur. Mauris nibh ante, rutrum ut massa vitae, finibus tincidunt ante. Aliquam euismod convallis ante, in rutrum libero aliquet vitae. Maecenas accumsan mi erat. Proin laoreet arcu diam, a ullamcorper erat efficitur vitae. Sed tincidunt ultricies dapibus. Sed imperdiet turpis id imperdiet accumsan. Ut a laoreet felis. Etiam sodales efficitur fermentum. Aenean ac nisi vitae ipsum bibendum lacinia. Phasellus finibus nisl tortor, ut pretium tellus pulvinar nec. Maecenas ut nibh sagittis quam tincidunt pharetra vel non turpis.
+<br>
+Fusce sit amet rutrum tortor, a condimentum dui. In vel elit commodo eros finibus malesuada. Aliquam iaculis, ex maximus scelerisque placerat, dui lacus laoreet libero, a aliquam eros quam vitae eros. Ut varius ultrices ligula non euismod. Praesent gravida dapibus felis, non posuere nisl malesuada vitae. Praesent pellentesque, est nec consequat condimentum, nisi nisl rutrum justo, id placerat leo ante id turpis. Curabitur placerat pretium nulla, id suscipit velit rutrum eget. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec hendrerit massa at orci laoreet porttitor. Etiam ex tortor, semper quis molestie sit amet, varius at erat.
+<br>
+Aliquam ut augue feugiat, lobortis orci nec, malesuada diam. Vestibulum quis massa at ipsum ornare suscipit. Donec a condimentum lectus. Vivamus laoreet quam laoreet quam commodo, vitae sodales ligula congue. Vivamus dapibus, sem quis feugiat tempus, odio ligula ultrices ex, vel sodales eros lorem et augue. In hac habitasse platea dictumst. Duis eu libero lacus. Proin nec hendrerit lacus. Sed hendrerit, velit in suscipit semper, arcu leo scelerisque risus, et interdum eros eros varius est. Suspendisse a justo rhoncus, molestie ligula ut, faucibus nunc.
+<br>
+Suspendisse semper dictum nisl, et posuere augue facilisis id. Proin ligula nisl, rhoncus a auctor quis, porttitor nec nisl. Proin porttitor sapien ante, at finibus erat lobortis at. Suspendisse ac diam facilisis, viverra risus at, auctor lacus. Praesent sed lectus bibendum, dignissim metus at, placerat lorem. Vivamus id est blandit, semper arcu vitae, vulputate tortor. Pellentesque non mauris et ipsum imperdiet commodo eget ut quam. Mauris vel purus eu neque ullamcorper ullamcorper eget ac mauris. Donec finibus mauris et mauris lacinia efficitur. Nulla ac blandit lorem. Aenean sodales facilisis mattis. Donec convallis urna eu enim scelerisque viverra. Sed id mollis nulla. Donec vehicula lobortis condimentum.
+<br>
+Praesent non faucibus enim. Praesent finibus vehicula malesuada. Vivamus sed maximus sem, et sodales dolor. Praesent suscipit magna id justo ullamcorper consectetur. Vivamus tempus ex risus, vel tempus ante mattis at. Donec imperdiet tempus diam, vel condimentum metus facilisis molestie. Mauris sit amet mauris ultrices, imperdiet ipsum non, ultrices mi. Vestibulum ultrices semper velit sit amet dapibus. Sed tempor ultrices urna. Vestibulum fringilla lacus quis turpis feugiat aliquet. Suspendisse ultrices lacinia dignissim. Duis dictum sapien lectus, nec finibus urna hendrerit vitae. Nunc eget mauris euismod, blandit lorem non, faucibus augue. Ut tortor risus, gravida vitae elit eu, convallis fermentum velit. Aliquam erat volutpat.
+<br>
+Etiam quis eleifend turpis, nec interdum libero. Fusce malesuada auctor leo, a volutpat nibh porta et. Duis at sagittis velit. Morbi ut pretium nisi. Pellentesque a vehicula magna. Aenean placerat ornare purus ut dictum. Aliquam a leo eget sapien accumsan porttitor nec ac nulla. Aenean pellentesque lorem ac sem auctor dapibus eu eu ligula. </div>
+ <script src="main.js"></script>
+</body>
+</html>
---
+++ js-navbar/main.js
@@ -0,0 +1,19 @@
+/* let prevScrollpos = window.pageYOffset;
+
+window.onscroll = slideDown;
+function slideDown() {
+ // Votre code ici
+    let currentScrollPos = window.pageYOffset;
+    if(prevScrollpos > currentScrollPos){
+        document.getElementById('navbar').style.top ='-56px';
+    } else{
+        document.getElementById('navbar').style.top ='0';
+    }
+    prevScrollpos < currentScrollPos;
+} */
+/* window.onscroll = () => { */
+window.onscroll=slideDown; 
+function slideDown() {
+    if(window.scrollY > 0)document.getElementById('navbar').style.top = "0";
+    else document.getElementById('navbar').style.top ="-56px";
+}
